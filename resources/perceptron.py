285b4d5cccc67305fa941de53a0a3beff7bb3663
Ariel <arielmorelli2@gmail.com>
2015-09-03 11:58:14 -0300

    First attempt to finish.

--- resources/perceptron.py
+++ resources/perceptron.py
@@ -1,7 +1,32 @@
-
-
 class Perceptron(object):
     def __init__(self, learn_rate, size):
         self.learn_rate = learn_rate
-        self.weights = [0]*(size*size)
+        self.size = size
+        self.weights = [0]*(size*size+1)
         print "learn rate:", self.learn_rate, "pesos:", self.weights
+
+    def learn(self, tests):
+        interation = 0
+        done = False
+        while not done:
+            interation += 1
+            done = True
+            function_value = 0
+            for test in tests:
+                for i in range(0, len(self.weights)):
+                    function_value += self.weights[i]*test['entry'][i]
+                s_out = self._f_value(function_value)
+                if s_out != test['expected']:
+                    done = False
+                    for i in range(0, len(self.weights)):
+                        self.weights[i] += self.learn_rate*(test['expected'] - s_out)*test['entry'][i]
+        print 'Iterations:', interation
+        # print self.weights.reshape(self.size, self.size)
+        for i in range(1, self.size*self.size+1, self.size):
+            print [self.weights[i+j] for j in range(0, self.size)]
+
+    @staticmethod
+    def _f_value(value):
+        if value > 0:
+            return 1
+        return 0
--- trabalho_1.py
+++ trabalho_1.py
@@ -2,9 +2,42 @@ from resources.perceptron import Perceptron
 
 
 def run_perceptron():
-    perceptron = Perceptron(0.5, 2)
+    tests = []
+    size = load_test_set(tests)
+    perceptron = Perceptron(0.5, size)
+    perceptron.learn(tests)
 
-# if __name__ == "__main__":
-run_perceptron()
+
+def load_test_set(tests):
+    file = open('infile.txt', 'r')
+    size = int(file.readline())
+
+    current = 0
+    matrix = []
+    for line in file:
+        if current < size:
+            matrix.append(line)
+            current += 1
+        else:
+            in_values = process_matrix(matrix)
+            expected_value = int(line)
+            test_entry = dict(entry=in_values, expected=expected_value)
+            tests.append(test_entry)
+            matrix= []
+            current = 0
+
+    return size
 
 
+def process_matrix(matrix):
+    vector = []
+    vector.append(1) # BIAS
+    for line in matrix:
+        for char in list(line[:-1]):
+            vector.append(int(char))
+
+    return vector
+
+
+if __name__ == "__main__":
+    run_perceptron()
